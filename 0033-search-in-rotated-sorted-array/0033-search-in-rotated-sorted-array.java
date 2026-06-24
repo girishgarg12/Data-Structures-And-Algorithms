43713f3dafdd042186371ee208fac127a90bffc7
Girish Garg <girish2nd77@gmail.com>
2026-06-24 11:24:10 +0530

    Time: 0 ms (100%), Space: 43.7 MB (64.61%) - LeetHub - Time: O(log n), Space: O(1)

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,17 +1,17 @@
 class Solution {
     public int search(int[] nums, int target) {
         int n = nums.length;
-        int low = 0, high = n-1;
-        while(low <= high){
-            int mid = low + (high-low)/2;
+        int l = 0, h = n-1;
+        while(l <= h){
+            int mid = l + (h-l)/2;
             if(nums[mid] == target) return mid;
-            if(nums[low] <= nums[mid]){
-                if(nums[low] <= target && target <= nums[mid])  high = mid-1;
-                else low = mid+1;
+            if(nums[mid] >= nums[l]){
+                if(target < nums[mid] && target >= nums[l]) h = mid-1;
+                else l = mid+1;
             }
             else{
-                if(target <= nums[high] && nums[mid] <= target) low = mid+1;
-                else high = mid-1;
+                if(target > nums[mid] && target <= nums[h]) l = mid+1;
+                else h = mid-1;
             }
         }
         return -1;
@@ -20,4 +20,4 @@ class Solution {
 
 // Synced seamlessly with LeetHub Pro
 // Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
-// Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
