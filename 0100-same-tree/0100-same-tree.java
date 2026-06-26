30802fdb3838fc71463d13b7cd2aa5b073f2e711
Girish Garg <girish2nd77@gmail.com>
2026-06-26 11:31:57 +0530

    Time: 0 ms (100%), Space: 42.7 MB (69.86%) - LeetHub - Time: O(n), Space: O(h)

---
+++ 0100-same-tree/0100-same-tree.java
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if(p == null && q == null) return true;
+        if(p == null || q == null) return false;
+        return (p.val == q.val) && isSameTree(p.left,q.left) && isSameTree(p.right, q.right);
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
