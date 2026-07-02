c1e3a55b39935f1959131dca9e111cc4fc33bcf5
Girish Garg <girish2nd77@gmail.com>
2026-07-02 12:03:57 +0530

    Time: 4 ms (63.97%), Space: 50.3 MB (37.67%) - LeetHub - Time: O(?), Space: O(?)

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -1,16 +1,16 @@
 class Solution {
-    public int helper(int[][] grid, int i, int j, int[][] memo){
-        if(i == grid.length || j == grid[0].length) return Integer.MAX_VALUE;
-        if(i == grid.length-1 && j == grid[0].length-1) return grid[i][j];
-        if(memo[i][j] != -1) return memo[i][j];
-        memo[i][j] = grid[i][j] + Math.min(helper(grid, i+1, j, memo), helper(grid, i, j+1, memo));
-        return memo[i][j];
-    }
     public int minPathSum(int[][] grid) {
         int n = grid.length, m = grid[0].length;
-        int[][] memo = new int[n][m];
-        for(int i = 0; i < n; i++) Arrays.fill(memo[i], -1);
-        return helper(grid, 0, 0, memo);
+        int[][] memo = new int[n+1][m+1];
+        memo[n-1][m-1]  = grid[n-1][m-1];
+        for(int i = n-2; i >= 0; i--) memo[i][m-1] = grid[i][m-1] + memo[i+1][m-1];
+        for(int j = m-2; j >= 0; j--) memo[n-1][j] = grid[n-1][j] + memo[n-1][j+1];
+        for(int i = n-2; i >= 0; i--){
+            for(int j = m-2 ; j >= 0; j--){
+                memo[i][j] = grid[i][j] + Math.min(memo[i+1][j], memo[i][j+1]);
+            }
+        }
+        return memo[0][0];
     }
 }
 
