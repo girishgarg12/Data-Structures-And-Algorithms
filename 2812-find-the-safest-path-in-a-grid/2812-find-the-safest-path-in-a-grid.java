33faa51ab5ea07902eaf700be3aa2db61ef70819
Girish Garg <girish2nd77@gmail.com>
2026-07-02 12:40:57 +0530

    Time: 181 ms (35%), Space: 121 MB (50%) - LeetHub - Time: O(n*m*log(v)), Space: O(n*m)

--- 2812-find-the-safest-path-in-a-grid/2812-find-the-safest-path-in-a-grid.java
+++ 2812-find-the-safest-path-in-a-grid/2812-find-the-safest-path-in-a-grid.java
@@ -7,6 +7,39 @@ class Solution {
             this.dis = dis;
         }
     }
+    class pair {
+        int row, col;
+        pair(int row, int col){
+            this.row = row;
+            this.col = col;
+        }
+    }
+    public int[] r = new int[]{-1,0,1,0};
+    public int[] c = new int[]{0,1,0,-1};
+
+    public boolean hasPath(int[][] dist, int v){
+        if(dist[0][0] < v) return false;
+        int n = dist.length, m = dist[0].length;
+        Queue<pair> q = new ArrayDeque<>();
+        boolean[][] visited = new boolean[n][m];
+        q.add(new pair(0, 0));
+        visited[0][0] = true;
+        while(!q.isEmpty()){
+            pair p = q.remove();
+            int row = p.row;
+            int col = p.col;
+            if(row == n-1 && col == m-1) return true; 
+            for(int i = 0; i < 4; i++){
+                int nRow = row + r[i];
+                int nCol = col + c[i];
+                if(nRow >= 0 && nRow < n && nCol >= 0 && nCol < m && !visited[nRow][nCol] && dist[nRow][nCol] >= v){
+                    visited[nRow][nCol] = true;
+                    q.add(new pair(nRow, nCol));
+                }
+            }
+        }
+        return false;
+    }
     public int maximumSafenessFactor(List<List<Integer>> grid) {
         int n = grid.size(), m = grid.get(0).size();
         Queue<truple> q = new ArrayDeque<>();
@@ -18,8 +51,7 @@ class Solution {
                 dist[i][j] = 0;
                 }   
         }
-        int[] r = new int[]{-1,0,1,0};
-        int[] c = new int[]{0,1,0,-1};
+        int max = 0;
         while(!q.isEmpty()){
             truple t = q.remove();
             int d = t.dis;
@@ -30,31 +62,18 @@ class Solution {
                 int nCol = col + c[i];
                 if(nRow >= 0 && nRow < n && nCol >= 0 && nCol < m && dist[nRow][nCol] > d+1){
                     dist[nRow][nCol] = d + 1;
+                    max = Math.max(dist[nRow][nCol], max);
                     q.add(new truple(d+1, nRow, nCol));
                 }
             }
         }
-        boolean[][] visited = new boolean[n][m];
-        PriorityQueue<truple> pq = new PriorityQueue<>((a,b) -> b.dis - a.dis);
-        pq.add(new truple(dist[0][0],0,0));
-
-        while(!pq.isEmpty()){
-            truple t = pq.remove();
-            int currSafe = t.dis;
-            int row = t.row;
-            int col = t.col;
-            if(row == n-1 && col == m-1) return currSafe;
-            for(int i = 0; i < 4; i++){
-                int nRow = row + r[i];
-                int nCol = col + c[i];
-                if(nRow >= 0 && nRow < n && nCol >= 0 && nCol < m && !visited[nRow][nCol]){
-                    visited[nRow][nCol] = true;
-                    int newSafe = Math.min(currSafe, dist[nRow][nCol]);
-                    pq.add(new truple(newSafe, nRow, nCol));
-                }
-            }
+        int l = 0, r = max;
+        while(l <= r){
+            int mid = l + (r-l)/2;
+            if(hasPath(dist, mid)) l = mid+1;
+            else r = mid-1;
         }
-        return -1;
+        return r;
     }
 }
 
