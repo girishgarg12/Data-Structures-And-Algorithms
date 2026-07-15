20a58341a267a8ba1bc3ecda33fd2646fd4ef191
Girish Garg <girish2nd77@gmail.com>
2026-07-15 14:49:48 +0530

    Time: 2 ms (94.34%), Space: 57.1 MB (30.26%) - LeetHub - Time: O(?), Space: O(?)

---
+++ 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int res = 0;
+    public void dfs(TreeNode root, int max){
+        if(root == null) return;
+        if(root.val >= max) res++;
+        dfs(root.left, Math.max(root.val, max));
+        dfs(root.right, Math.max(root.val, max));
+    }
+    public int goodNodes(TreeNode root) {
+        dfs(root, Integer.MIN_VALUE);
+        return res;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
